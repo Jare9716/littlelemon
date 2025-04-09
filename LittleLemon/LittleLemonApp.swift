b32419cb01f7c689a7626f7af103caeb52894e8d
Jare9716 <jare9716@gmail.com>
2025-04-09 18:02:25 -0500

    core data created

--- LittleLemon/LittleLemonApp.swift
+++ LittleLemon/LittleLemonApp.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 @main
 struct LittleLemonApp: App {
-    let persistenceController = PersistenceController.shared
+//    let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
--- LittleLemon/Menu.swift
+++ LittleLemon/Menu.swift
@@ -8,15 +8,63 @@
 import SwiftUI
 
 struct Menu: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    
+    func getMenuData() {
+        PersistenceController.shared.clear()
+        
+        let serverURLString =
+        "https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu.json"
+        
+        let url = URL(string: serverURLString)!
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request) {
+            data,
+            response,
+            error in
+            
+            if let data = data {
+                let decoder = JSONDecoder()
+                
+                if let menuList = try? decoder.decode(MenuList.self, from: data)
+                {
+                    for menuItem in menuList.menu {
+                        let dish = Dish(context: viewContext)
+                        dish.title = menuItem.title
+                        dish.image = menuItem.image
+                        dish.price = menuItem.price
+                    }
+                    
+                    try? viewContext.save().self
+                }
+            }
+        }
+        task.resume()
+    }
+    
     var body: some View {
         VStack {
             Text("Little Lemon")
             Text("Chicago")
             Text("An app for managing ordering your favorite food")
-            List{
-                
+            
+            FetchedObjects { (dishes: [Dish]) in
+                List {
+                    ForEach(dishes) { dish in
+                        HStack {
+                            Text("\(dish.title!) - $\(dish.price!)")
+                            AsyncImage(url: URL(string: dish.image!)) { image in
+                                image.resizable()
+                                    .frame(width: 100, height: 100)
+                            } placeholder: {
+                                ProgressView()
+                            }
+                        }
+                    }
+                }
             }
         }
+        .onAppear { getMenuData() }
     }
 }
 
